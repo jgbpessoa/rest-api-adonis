b6fa44e274831f7cbda172d8d2b1cbf7cb3ab0ef
jgbpessoa <jgbpessoa@gmail.com>
2022-07-06 12:37:43 -0300

    POST route (image upload)

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,11 +1,32 @@
 // Necessary to get request info (route, body, etc...)
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import { v4 as uuidv4 } from 'uuid'
+
 import Moment from 'App/Models/Moment'
 
+import Application from '@ioc:Adonis/Core/Application'
+
 export default class MomentsController {
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
+
+    const image = request.file('image', this.validationOptions)
+
+    if (image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      body.image = imageName
+    }
 
     const moment = await Moment.create(body)
 
