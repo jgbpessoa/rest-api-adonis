00d7a67a7d4f975050f1cf39882888cdbc744be1
jgbpessoa <jgbpessoa@gmail.com>
2022-07-06 14:41:09 -0300

    Comment POST route (CommentsController.store)

---
+++ app/Controllers/Http/CommentsController.ts
@@ -0,0 +1,24 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Comment from 'App/Models/Comment'
+import Moment from 'App/Models/Moment'
+
+export default class CommentsController {
+  public async store({ params, request, response }: HttpContextContract) {
+    const body = request.body()
+
+    const momentId = params.momentId
+
+    await Moment.findOrFail(momentId)
+
+    body.momentId = momentId
+
+    const comment = await Comment.create(body)
+
+    response.status(201)
+
+    return {
+      message: 'Comment was successfully added',
+      data: comment,
+    }
+  }
+}
